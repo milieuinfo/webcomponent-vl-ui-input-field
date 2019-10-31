ccce205f2f716a18e46096fea0c37cc8aedf0741
Tom Coemans <tom.coemans@gmail.com>
2019-10-31 11:33:12 +0100

    alleen formulier validatie type attributen toevoegen indien het input veld onderdeel is van een formulier

--- vl-input-field.src.js
+++ vl-input-field.src.js
@@ -23,8 +23,6 @@ export class VlInputField extends VlFormValidation(NativeVlElement(HTMLInputElem
 
   connectedCallback() {
     this.classList.add('vl-input-field');
-    this.setAttribute('data-vl-success-class', 'vl-input-field--success');
-    this.setAttribute('data-vl-error-class', 'vl-input-field--error');
     this._dress();
   }
 
@@ -38,6 +36,8 @@ export class VlInputField extends VlFormValidation(NativeVlElement(HTMLInputElem
 
   _dress() {
     if (this.form) {
+      this.setAttribute('data-vl-success-class', 'vl-input-field--success');
+      this.setAttribute('data-vl-error-class', 'vl-input-field--error');
       this.dress(this.form);
     }
   }
