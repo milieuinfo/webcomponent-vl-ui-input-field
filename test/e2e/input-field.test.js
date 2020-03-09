3ef85d5ca179fa8bf279cc1cf21d9420c9958a71
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-03-09 09:06:29 +0100

    UIG-368 inputfield E2E tests

--- test/e2e/input-field.test.js
+++ test/e2e/input-field.test.js
@@ -15,57 +15,38 @@ describe('vl-input-field', async () => {
         await assert.eventually.equal(inputField.getInputValue(), inputText);
     });
 
-    it('Als gebruiker zie ik een gewoon inputfield', async() => {
-        const inputField = await vlInputFieldPage.getInputField();
-        await assert.eventually.isTrue(inputField.isEnabled());
-        await assert.eventually.isFalse(inputField.isBlock());
-        await assert.eventually.isFalse(inputField.isError());
-        await assert.eventually.isFalse(inputField.isSuccess());
-        await assert.eventually.isFalse(inputField.isSmall());
+    it('Als gebruiker zie ik het onderscheid tussen een gewoon inputfield en een block inputfield', async() => {
+        const inputFieldNotBlock = await vlInputFieldPage.getInputField();
+        await assert.eventually.isFalse(inputFieldNotBlock.isBlock());
+        const inputFieldBlock = await vlInputFieldPage.getInputFieldBlock();
+        await assert.eventually.isTrue(inputFieldBlock.isEnabled());
     });
 
-    it('Als gebruiker zie ik een block inputfield', async() => {
-        const inputField = await vlInputFieldPage.getInputFieldBlock();
-        await assert.eventually.isTrue(inputField.isEnabled());
-        await assert.eventually.isTrue(inputField.isBlock());
-        await assert.eventually.isFalse(inputField.isError());
-        await assert.eventually.isFalse(inputField.isSuccess());
-        await assert.eventually.isFalse(inputField.isSmall());
+    it('Als gebruiker zie ik het onderscheid tussen een gewoon inputfield en een error inputfield', async() => {
+    	const inputFieldNotError = await vlInputFieldPage.getInputField();
+    	await assert.eventually.isFalse(inputFieldNotError.isBlock());
+        const inputFieldError = await vlInputFieldPage.getInputFieldError();
+        await assert.eventually.isTrue(inputFieldError.isError());
     });
 
-    it('Als gebruiker zie ik een error inputfield', async() => {
-        const inputField = await vlInputFieldPage.getInputFieldError();
-        await assert.eventually.isTrue(inputField.isEnabled());
-        await assert.eventually.isTrue(inputField.isError());
-        await assert.eventually.isFalse(inputField.isBlock());
-        await assert.eventually.isFalse(inputField.isSuccess());
-        await assert.eventually.isFalse(inputField.isSmall());
+    it('Als gebruiker zie ik het onderscheid tussen een gewoon inputfield en een success inputfield', async() => {
+    	const inputFieldNotSuccess = await vlInputFieldPage.getInputField();
+    	await assert.eventually.isFalse(inputFieldNotSuccess.isSuccess());
+        const inputFieldSuccess = await vlInputFieldPage.getInputFieldSuccess();
+        await assert.eventually.isTrue(inputFieldSuccess.isSuccess());
     });
 
-    it('Als gebruiker zie ik een success inputfield', async() => {
-        const inputField = await vlInputFieldPage.getInputFieldSuccess();
-        await assert.eventually.isTrue(inputField.isEnabled());
-        await assert.eventually.isTrue(inputField.isSuccess());
-        await assert.eventually.isFalse(inputField.isError());
-        await assert.eventually.isFalse(inputField.isBlock());
-        await assert.eventually.isFalse(inputField.isSmall());
+   	it('Als gebruiker zie ik het onderscheid tussen een gewoon inputfield en een disabled inputfield', async() => {
+    	const inputFieldEnabled = await vlInputFieldPage.getInputField();
+    	await assert.eventually.isTrue(inputFieldEnabled.isEnabled());
+        const inputFieldNotEnabled = await vlInputFieldPage.getInputFieldDisabled();
+        await assert.eventually.isFalse(inputFieldNotEnabled.isEnabled());
     });
 
-    it('Als gebruiker zie ik een disabled inputfield', async() => {
-        const inputField = await vlInputFieldPage.getInputFieldDisabled();
-        await assert.eventually.isFalse(inputField.isEnabled());
-        await assert.eventually.isFalse(inputField.isSuccess());
-        await assert.eventually.isFalse(inputField.isError());
-        await assert.eventually.isFalse(inputField.isBlock());
-        await assert.eventually.isFalse(inputField.isSmall());
-    });
-
-    it('Als gebruiker zie ik een small inputfield', async() => {
-        const inputField = await vlInputFieldPage.getInputFieldSmall();
-        await assert.eventually.isTrue(inputField.isEnabled());
-        await assert.eventually.isTrue(inputField.isSmall());
-        await assert.eventually.isFalse(inputField.isSuccess());
-        await assert.eventually.isFalse(inputField.isError());
-        await assert.eventually.isFalse(inputField.isBlock());
+   	it('Als gebruiker zie ik het onderscheid tussen een gewoon inputfield en een small inputfield', async() => {
+   		const inputFieldNotSmall = await vlInputFieldPage.getInputField();
+   		await assert.eventually.isFalse(inputFieldNotSmall.isSmall());
+        const inputFieldSmall = await vlInputFieldPage.getInputFieldSmall();
+        await assert.eventually.isTrue(inputFieldSmall.isSmall());
     });
 });
