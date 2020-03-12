d3b3dbe7fb92ace0d68197bf39f519720235d7f1
wautergu <42434700+wautergu@users.noreply.github.com>
2020-03-12 14:11:29 +0100

    E2E test verbeteringen (#61)

--- test/e2e/components/vl-input-field.js
+++ test/e2e/components/vl-input-field.js
@@ -1,6 +1,32 @@
 const { VlElement } = require('vl-ui-core').Test;
+const { VlFormValidation } = require('vl-ui-form-validation').Test;
 
-class VlInputField extends VlElement {  
+class VlInputField extends VlFormValidation { 
+    
+    async setValue(content) {
+    	await this.clear();
+        return this.sendKeys(content);
+    }
+
+    async getValue() {
+        return this.getAttribute('value');
+    }
+
+    async isBlock() {
+        return this.hasAttribute('block');
+    }
+
+    async isError() {
+        return this.hasAttribute('error');
+    }
+
+    async isSuccess() {
+        return this.hasAttribute('success');
+    }
+
+    async isSmall() {
+        return this.hasAttribute('small');
+    }
 }
 
 module.exports = VlInputField;
