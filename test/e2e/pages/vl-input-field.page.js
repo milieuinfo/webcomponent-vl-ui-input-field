72ffde3112eea2c1b7c6a1151fa4debe3b3fac32
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 13:58:04 +0200

    ESlint

--- test/e2e/pages/vl-input-field.page.js
+++ test/e2e/pages/vl-input-field.page.js
@@ -1,68 +1,68 @@
 const VlInputField = require('../components/vl-input-field');
-const { Page, Config } = require('vl-ui-core').Test;
-const { By } = require('vl-ui-core').Test.Setup;
+const {Page, Config} = require('vl-ui-core').Test;
+const {By} = require('vl-ui-core').Test.Setup;
 
 class VlInputFieldPage extends Page {
-	async _getInputField(selector) {
-		return new VlInputField(this.driver, selector);
-	}
+  async _getInputField(selector) {
+    return new VlInputField(this.driver, selector);
+  }
 
-	async getInputField() {
-		return this._getInputField('#input-field');
-	}
+  async getInputField() {
+    return this._getInputField('#input-field');
+  }
 
-	async getInputFieldBlock() {
-		return this._getInputField('#input-field-block');
-	}
+  async getInputFieldBlock() {
+    return this._getInputField('#input-field-block');
+  }
 
-	async getInputFieldError() {
-		return this._getInputField('#input-field-error');
-	}
+  async getInputFieldError() {
+    return this._getInputField('#input-field-error');
+  }
 
-	async getInputFieldSuccess() {
-		return this._getInputField('#input-field-success');
-	}
+  async getInputFieldSuccess() {
+    return this._getInputField('#input-field-success');
+  }
 
-	async getInputFieldDisabled() {
-		return this._getInputField('#input-field-disabled');
-	}
+  async getInputFieldDisabled() {
+    return this._getInputField('#input-field-disabled');
+  }
 
-	async getInputFieldSmall() {
-		return this._getInputField('#input-field-small');
-	}
+  async getInputFieldSmall() {
+    return this._getInputField('#input-field-small');
+  }
 
-	async getInputFieldVoornaam() {
-		return this._getInputField('#input-voornaam');
-	}
+  async getInputFieldVoornaam() {
+    return this._getInputField('#input-voornaam');
+  }
 
-	async getInputFieldIban() {
-		return this._getInputField('#input-iban');
-	}
+  async getInputFieldIban() {
+    return this._getInputField('#input-iban');
+  }
 
-	async getInputFieldVoornaamErrorMessage() {
-		return this.driver.findElement(By.css('#validation-voornaam-error-message'));
-	}
+  async getInputFieldVoornaamErrorMessage() {
+    return this.driver.findElement(By.css('#validation-voornaam-error-message'));
+  }
 
-	async getInputFieldIbanErrorMessage() {
-		return this.driver.findElement(By.css('#validation-iban-error-message'));
-	}
+  async getInputFieldIbanErrorMessage() {
+    return this.driver.findElement(By.css('#validation-iban-error-message'));
+  }
 
-	async validateForm() {
-		const validateFormButton = await this.driver.findElement(By.css("#form-validate-button"));
-		return validateFormButton.click();
-	}
+  async validateForm() {
+    const validateFormButton = await this.driver.findElement(By.css('#form-validate-button'));
+    return validateFormButton.click();
+  }
 
-	async getInputFieldMetEvent() {
-		return this._getInputField('#input-field-met-event');
-	}
+  async getInputFieldMetEvent() {
+    return this._getInputField('#input-field-met-event');
+  }
 
-	async getInputFieldCopy() {
-		return this._getInputField('#input-field-copy');
-	}
-	
-	async load() {
-		await super.load(Config.baseUrl + '/demo/vl-input-field.html');
-	}
+  async getInputFieldCopy() {
+    return this._getInputField('#input-field-copy');
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-input-field.html');
+  }
 }
 
 module.exports = VlInputFieldPage;
