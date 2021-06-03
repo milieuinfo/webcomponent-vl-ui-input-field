e1ba00e80aa6c9e6f86bc7516770cdedfe5c8152
coemans <tom.coemans@zeuzios.be>
2021-06-03 15:25:48 +0200

    Update dependencies (#109)

--- test/e2e/pages/vl-input-field.page.js
+++ test/e2e/pages/vl-input-field.page.js
@@ -1,6 +1,5 @@
 const VlInputField = require('../components/vl-input-field');
 const {Page, Config} = require('vl-ui-core').Test;
-const {By} = require('vl-ui-core').Test.Setup;
 
 class VlInputFieldPage extends Page {
   async getInputField() {
@@ -25,35 +24,6 @@ class VlInputFieldPage extends Page {
 
   async getInputFieldSmall() {
     return this._getInputField('#input-field-small');
-  }
-
-  async getInputFieldVoornaam() {
-    return this._getInputField('#input-voornaam');
-  }
-
-  async getInputFieldIban() {
-    return this._getInputField('#input-iban');
-  }
-
-  async getInputFieldVoornaamErrorMessage() {
-    return this.driver.findElement(By.css('#validation-voornaam-error-message'));
-  }
-
-  async getInputFieldIbanErrorMessage() {
-    return this.driver.findElement(By.css('#validation-iban-error-message'));
-  }
-
-  async validateForm() {
-    const validateFormButton = await this.driver.findElement(By.css('#form-validate-button'));
-    await validateFormButton.click();
-  }
-
-  async getInputFieldMetEvent() {
-    return this._getInputField('#input-field-met-event');
-  }
-
-  async getInputFieldCopy() {
-    return this._getInputField('#input-field-copy');
   }
 
   async load() {
